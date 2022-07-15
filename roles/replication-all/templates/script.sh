3235392b3106ebd2b8c15312eb26e5fe0af91e6c
devops-int <easpa.softserveinc.com>
2022-07-15 18:26:40 +0300

    new deploy

---
+++ roles/replication-all/templates/script.sh
@@ -0,0 +1,12 @@
+DATE=`date +"%d_%b_%Y_%H%M"`
+SQLFILE=/var/backup/db/db_backup_${DATE}.sql
+DATABASE=Check_db
+USER=root
+PASSWORD="MyNewPass1!"
+ 
+unalias rm     2> /dev/null
+rm ${SQLFILE}     2> /dev/null
+rm ${SQLFILE}.gz  2> /dev/null
+ 
+
+sudo mysqldump -u ${USER} -p${PASSWORD} ${DATABASE}|gzip > ${SQLFILE}.gz
